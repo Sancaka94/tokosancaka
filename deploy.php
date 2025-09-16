eeb683773d65f6078ba1ccf8ce9f63f5aa56bd13
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-09-16 11:39:11 +0700

    perbaiki deploy

--- deploy.php
+++ deploy.php
@@ -1,10 +1,10 @@
 <?php
 // Lokasi folder project di server
-$path = "/home/username/public_html/tokosancaka";
-$log_file = "/home/username/deploy.log";
+$path = "/home/tokq3391/public_html/";
+$log_file = "/home/tokq3391/deploy.log";
 
 // Jalankan git pull
-exec("cd {$path} && git pull origin main 2>&1", $output);
+exec("cd {$path} && git pull origin master 2>&1", $output);
 
 // Simpan log
 file_put_contents($log_file, date('Y-m-d H:i:s') . "\n" . implode("\n", $output) . "\n\n", FILE_APPEND);
