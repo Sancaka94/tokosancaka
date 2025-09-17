3aa57f984ae913498ca2f2e0842dc905af6282de
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-09-17 22:42:14 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 
 
-@section('title', 'Selamat Datang di Sancaka Express - Solusi Pengiriman Terlengkap & Terpercaya')
+@section('title', 'Selamat Datang di Sancaka Express - Solusi Pengiriman Terlengkap, Terpercaya & Terjangkau')
 
 
 
