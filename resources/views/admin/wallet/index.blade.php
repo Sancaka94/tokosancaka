bb3614f7c1e45fb6cdaadfa0c23edf6019bddfc7
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-09-20 17:49:05 +0700

    WIP

--- resources/views/admin/wallet/index.blade.php
+++ resources/views/admin/wallet/index.blade.php
@@ -80,7 +80,7 @@
                 </tr>
             </thead>
             <tbody>
-                @forelse ($pelanggan as $user)
+                @forelse ($pengguna as $user)
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $user->id }}</td>
                     <td class="py-4 px-6">{{ $user->nama_lengkap }}</td>
