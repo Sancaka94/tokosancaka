dfe09a6a9392dceb91a8c69a2a1b557df54a4a57
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-09-21 22:27:51 +0700

    WIP

--- resources/views/admin/pesanan/cetak_thermal.blade.php
+++ resources/views/admin/pesanan/cetak_thermal.blade.php
@@ -151,7 +151,9 @@
                     <p class="label">Rincian Paket:</p>
                     <p class="value">- Berat: {{ $pesanan->weight }} gr</p>
                     <p class="value">- Dimensi: {{ $pesanan->length ?? 0 }}x{{ $pesanan->width ?? 0 }}x{{ $pesanan->height ?? 0 }} cm</p>
-                    <p class="value">- Layanan: {{ strtoupper($pesanan->service_type) }}</p>
+                    <p class="value">- Layanan: {{ strtoupper($pesanan->service_type) }}</p><br><br>
+
+                    <p class="label">Nomor Resi: {{ $pesanan->resi }} </p>
                 </div>
             </div>
 
