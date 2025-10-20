e586326083a9a314a70c29031bbf2d65ea793ab3
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-10-21 06:43:10 +0700

    WIP

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,48 +1,41 @@
 <?php
 
-
-
 namespace App\Models;
 
-
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
 use Illuminate\Database\Eloquent\Model;
 
-
-
 class Category extends Model
-
 {
-
     use HasFactory;
 
-
-
-    protected $fillable = ['name', 'slug', 'user_id'];
-
-
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+        'icon', // Untuk ikon kategori marketplace
+        'type', // Untuk membedakan tipe (e.g., 'marketplace', 'blog')
+        'user_id' // Untuk kategori blog
+    ];
 
     /**
-
-     * Mendefinisikan relasi one-to-many ke model Post.
-
+     * Mendefinisikan relasi one-to-many ke model Post (untuk Blog).
      */
-
     public function posts()
-
     {
-
         return $this->hasMany(Post::class);
-
     }
 
+    /**
+     * Mendefinisikan relasi one-to-many ke model Product (untuk Marketplace).
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
 
-public function products()
-{
-    return $this->hasMany(Product::class);
-}
-
-}
