6a2aa97cd7ec9865f7967b7cf24caf106265190f
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-10-23 09:17:51 +0700

    WIP

--- routes/web.php
+++ routes/web.php
@@ -353,6 +353,15 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('categories/{category}/attributes', [ProductController::class, 'getAttributes'])->name('categories.attributes');
 
 
+                   // Route resource utama
+    Route::resource('products', ProductController::class);
+
+    // Route khusus tambahan
+    Route::post('/products/{product}/restock', [ProductController::class, 'restock'])->name('products.restock');
+    Route::patch('/products/{product}/out-of-stock', [ProductController::class, 'markAsOutOfStock'])->name('products.outOfStock');
+
+   
+
 });
 
 
@@ -417,14 +426,7 @@ Route::middleware(['auth', RoleMiddleware::class . ':Admin'])
 
             Route::delete('/{id}', [WilayahController::class, 'destroy'])->name('destroy');
 
-                // Route resource utama
-    Route::resource('products', ProductController::class);
-
-    // Route khusus tambahan
-    Route::post('/products/{product}/restock', [ProductController::class, 'restock'])->name('products.restock');
-    Route::patch('/products/{product}/out-of-stock', [ProductController::class, 'markAsOutOfStock'])->name('products.outOfStock');
-
-   
+ 
 
         });
 
