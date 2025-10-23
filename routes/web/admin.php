25d34df47b2c02f1eb33a0d9554d4e3f9491b776
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-10-23 10:27:36 +0700

    WIP

--- routes/web/admin.php
+++ routes/web/admin.php
@@ -178,11 +178,11 @@ Route::resource('customers', CustomerController::class);
          ->name('products.outOfStock');
 
       
-    Route::get('/{product}/{id}/edit', [ProductController::class, 'edit'])
+    Route::get('products/{id}/edit', [ProductController::class, 'edit'])
          ->name('products.edit');
 
     // Dihapus: Menggunakan {id} dan bertentangan dengan rute kustom di atas
-     Route::post('/{product}/{id}/restock', [ProductController::class, 'restock'])
+     Route::post('products/{id}/restock', [ProductController::class, 'restock'])
         ->name('products.restock');
 
     Route::resource('products', ProductController::class);
