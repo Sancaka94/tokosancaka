ccc30f0fc43d5b4810e73c6d0ff50691345e0ec0
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-10-23 10:00:13 +0700

    WIP

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -129,7 +129,7 @@
 
 @section('content')
 @include('layouts.partials.notifications')
-
+dd($product);
 <form id="product-form" action="{{ route('admin.products.update', $product) }}" method="POST" enctype="multipart/form-data" novalidate>
 
     @csrf
