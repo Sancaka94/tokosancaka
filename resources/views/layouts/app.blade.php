056f81c755de85e7ef9b121a7a69c1aececa2d66
Sancaka94 <TOKOSANCAKA@GMAIL.COM>
2025-11-21 11:48:24 +0700

    Auto sync from VSCode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -150,12 +150,12 @@
             <div class="container">
                 <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
                     <img src="https://tokosancaka.com/storage/uploads/sancaka.png" alt="Sancaka Express Logo" style="max-height: 40px;" class="me-2">
-                    <strong>SANCAKA EXPRESS</strong>
+                    <strong>SANCAKA EXPRESS HUTAMA</strong>
                 </a>
 
                 <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button>S
 
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto align-items-lg-center">
