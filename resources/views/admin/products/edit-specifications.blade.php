01da741149f2d01f058f8f8982a81c83d7fbc434
Admin Sancaka <admin@tokosancaka.com>
2025-11-29 21:15:23 +0700

    auto update

--- resources/views/admin/products/edit-specifications.blade.php
+++ resources/views/admin/products/edit-specifications.blade.php
@@ -207,7 +207,7 @@
         </div>
 
         {{-- FOOTER --}}
-        <div class="fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-200 shadow-lg py-4 md:pl-[280px]">
+        <div class="bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-200 shadow-lg py-4 md:pl-[280px]">
             <div class="max-w-7xl mx-auto flex justify-end gap-3 px-4">
 
                 <a href="{{ route('admin.products.edit', $product->slug) }}"
