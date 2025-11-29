3c381faf43f71a542856addbdc992037dc3d3d03
Admin Sancaka <admin@tokosancaka.com>
2025-11-29 17:16:39 +0700

    auto update

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -416,7 +416,7 @@
                     </div>
 
                     <a href="{{ route('admin.products.edit.specifications', $product->id) }}" 
-                       class="w-full inline-flex justify-center items-center px-4 py-2.5 bg-white border-2 border-indigo-100 text-indigo-600 font-semibold rounded-lg hover:bg-indigo-50 hover:border-indigo-200 transition-colors">
+                       class="w-full inline-flex justify-center items-center px-4 py-2.5 bg-green-300 border-2 border-indigo-100 text-indigo-600 font-semibold rounded-lg hover:bg-indigo-50 hover:border-indigo-200 transition-colors">
                         <i class="fa-solid fa-sliders mr-2"></i>
                         Edit Kategori & Spesifikasi
                     </a>
