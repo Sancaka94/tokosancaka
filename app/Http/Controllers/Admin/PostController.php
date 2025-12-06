a25b276252f36a5d724c0a874ad3d42c03b81a2d
Admin Sancaka <admin@tokosancaka.com>
2025-12-06 21:52:53 +0700

    auto update

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -61,7 +61,7 @@ class PostController extends Controller
     public function show(Post $post)
     {
         $post->load('tags', 'author', 'category'); 
-        return view('admin.posts.show', compact('post'));
+        return view('posts.show', compact('post'));
     }
 
     /**
