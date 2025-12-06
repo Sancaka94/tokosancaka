60de895068046c5560b85fd25c871726463d9b6d
Admin Sancaka <admin@tokosancaka.com>
2025-12-06 21:32:28 +0700

    auto update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -115,13 +115,7 @@ class Post extends Model
         return $query->with('category', 'author');
     }
 
-    /**
-     * Mendefinisikan kolom untuk Route Model Binding (untuk URL slug).
-     */
-    public function getRouteKeyName(): string
-    {
-        return 'slug';
-    }
+
 
 }
 
