f870dcc0c23ef319b7e5306010c207fe1ae28a83
Admin Sancaka <admin@tokosancaka.com>
2025-12-08 13:00:39 +0700

    auto update

--- resources/views/admin/ppob/index.blade.php
+++ resources/views/admin/ppob/index.blade.php
@@ -5,7 +5,7 @@
 {{-- Load SweetAlert2 CDN --}}
 @push('styles')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-@endpush
+@endpus
 
 @section('content')
 
