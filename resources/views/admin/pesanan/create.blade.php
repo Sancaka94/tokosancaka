0b68a9035c34124e10ff569ee6f94950ee4e82b4
Admin Sancaka <admin@tokosancaka.com>
2025-12-10 07:00:56 +0700

    auto update

--- resources/views/admin/pesanan/create.blade.php
+++ resources/views/admin/pesanan/create.blade.php
@@ -65,7 +65,10 @@
                             <label for="sender_name" class="block mb-2 text-sm font-medium text-gray-700">Nama Pengirim</label>
                             <input type="text" id="sender_name" name="sender_name"
                             class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5
-                            focus:outline-none focus:ring-0 focus:border-red-500"required autocomplete="off">
+       focus:outline-none focus:border-red-500
+       focus:ring-4 focus:ring-red-300
+       focus:shadow-md"
+required autocomplete="off">
                             <div id="sender_contact_results" class="search-results-container hidden"></div>
                         </div>
                         <div class="relative">
