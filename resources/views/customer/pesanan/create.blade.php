d5d1f7a39772dfe5108cc88a06ee9d75dc243808
Admin Sancaka <admin@tokosancaka.com>
2025-12-10 08:04:28 +0700

    auto update

--- resources/views/customer/pesanan/create.blade.php
+++ resources/views/customer/pesanan/create.blade.php
@@ -171,7 +171,15 @@
                             <i class="fas fa-map-marker-alt text-white mr-2"></i>Informasi Penerima
                         </h3>
                        <div class="relative w-1/2">
-                            <input type="search" id="receiver_contact_search" class="w-full pl-10 pr-4 py-2 bg-white border border-gray-300 rounded-lg text-sm text-gray-900 focus:outline-none focus:border-blue-500 focus:ring-4 focus:ring-blue-300 focus:shadow-md" placeholder="Cari dari kontak penerima..." autocomplete="off">
+                            <input type="search" id="receiver_contact_search" 
+                            class="w-full pl-10 pr-4 py-2 bg-white border border-gray-300 rounded-lg text-sm text-gray-900
+       transition-all duration-200
+       hover:border-blue-400 hover:ring-2 hover:ring-blue-200 hover:shadow-md
+       focus:outline-none
+       focus:border-blue-500
+       focus:ring-4 focus:ring-blue-300
+       focus:shadow-md"
+ placeholder="Cari dari kontak penerima..." autocomplete="off">
                            <div class="absolute top-0 left-0 inline-flex items-center p-2 h-full text-gray-400">
                                 <i class="fas fa-search"></i>
                             </div>
