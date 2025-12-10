63b434b0301409040fe8a34ec9acadd7f615a8af
Admin Sancaka <admin@tokosancaka.com>
2025-12-10 07:49:18 +0700

    auto update

--- resources/views/customer/pesanan/create.blade.php
+++ resources/views/customer/pesanan/create.blade.php
@@ -98,7 +98,7 @@
                         </h3>
                         <div class="relative w-1/2">
                             <input type="search" id="sender_contact_search"
-                            class="w-full pl-10 pr-4 py-2 bg-white border border-gray-300 rounded-lg text-sm text-gray-900 focus:outline-none focus:border-red-500 focus:ring-4 focus:ring-red-300 focus:shadow-md" placeholder="Cari dari kontak pengirim..." autocomplete="off">
+                            class="w-full pl-10 pr-4 py-2 bg-white border border-gray-300 rounded-lg text-sm text-gray-900 focus:outline-none focus:border-red-500 focus:ring-4 focus:ring-white focus:shadow-md" placeholder="Cari dari kontak pengirim..." autocomplete="off">
 
                            <div class="absolute top-0 left-0 inline-flex items-center p-2 h-full text-gray-400">
                                 <i class="fas fa-search"></i>
