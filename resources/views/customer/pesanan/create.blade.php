8113e07480a997ae43f08fef67a55efd1ad72061
Admin Sancaka <admin@tokosancaka.com>
2025-12-10 09:28:53 +0700

    auto update

--- resources/views/customer/pesanan/create.blade.php
+++ resources/views/customer/pesanan/create.blade.php
@@ -1032,7 +1032,7 @@ function type() {
     index++;
 
     if (index <= text.length) {
-        setTimeout(type, 150); // kecepatan mengetik
+        setTimeout(type, 90); // kecepatan mengetik
     } else {
         setTimeout(() => {
             index = 0;
