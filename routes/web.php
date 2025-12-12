3d0fabe98bfd6bbfed3452964b0de9b2b656cbbd
Admin Sancaka <admin@tokosancaka.com>
2025-12-12 19:31:59 +0700

    auto update

--- routes/web.php
+++ routes/web.php
@@ -153,8 +153,8 @@ Route::get('/marketplace/category/{category:slug}', [CategoryController::class, 
 Route::get('/pelanggan', [PublicPelangganController::class, 'index'])->name('pelanggan.public.index');
 
     // Fitur Multi Koli Admin (YANG BARU ANDA BUAT)
-    Route::get('/pesanan/buat-multi', [KoliController::class, 'create'])->name('admin.pesanan.create_multi');
-    Route::post('/pesanan/store-multi', [KoliController::class, 'store'])->name('koli.store');
+    Route::get('/admin/pesanan/buat-multi', [KoliController::class, 'create'])->name('admin.pesanan.create_multi');
+    Route::post('/admin/pesanan/store-multi', [KoliController::class, 'store'])->name('koli.store');
     Route::post('/pesanan/store-single', [KoliController::class, 'storeSingle'])->name('koli.store_single');
     Route::post('/cek-ongkir', [KoliController::class, 'cek_Ongkir'])->name('koli.cek_ongkir');
 
