54bd02edf37126a07b31d1ed8b983a8d915415b1
Admin Sancaka <admin@tokosancaka.com>
2025-12-13 22:40:12 +0700

    auto update

--- routes/web.php
+++ routes/web.php
@@ -198,6 +198,7 @@ Route::post('/ppob/check-pln-prabayar', [PpobController::class, 'checkPlnPrabaya
 // Blog & Content
 Route::get('/feed', [BlogController::class, 'generateFeed'])->name('feed');
 Route::get('/blog/posts/{slug}', [BlogController::class, 'show'])->name('blog.show');
+Route::get('/blog/posts/{slug}', [BlogController::class, 'show'])->name('blog.posts.show');
 Route::get('/pondok', [PondokController::class, 'index'])->name('pondok.index');
 
 // Utilities
