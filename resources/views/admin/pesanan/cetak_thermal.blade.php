1947af74a9b547cddea9543f5ec765a267a4f1f9
Admin Sancaka <admin@tokosancaka.com>
2025-12-20 01:05:21 +0700

    auto update

--- resources/views/admin/pesanan/cetak_thermal.blade.php
+++ resources/views/admin/pesanan/cetak_thermal.blade.php
@@ -164,7 +164,7 @@
                     <p class="value">- Isi Paket: {{ $pesanan->item_description }}</p>
                     <p class="value">- Dimensi: {{ $pesanan->length ?? 0 }} x {{ $pesanan->width ?? 0 }} x {{ $pesanan->height ?? 0 }} cm</p>
                     <p class="value">- Layanan: {{ strtoupper($expeditionService) }}</p><br>
-                    <p class="label text-red-500"><strong>Total Ongkir:</strong></p>
+                    <p class="label text-green-600"><strong>Total Ongkir:</strong></p>
 
 <p class="value text-red-600 text-lg">
     <strong>
