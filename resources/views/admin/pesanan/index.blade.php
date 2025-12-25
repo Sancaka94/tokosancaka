3909014f814cef734758fcfffe5a7ae11fda62bd
Admin Sancaka <admin@tokosancaka.com>
2025-12-25 17:30:58 +0700

    auto update

--- resources/views/admin/pesanan/index.blade.php
+++ resources/views/admin/pesanan/index.blade.php
@@ -92,7 +92,7 @@
         </div>
         <div class="flex items-center gap-2 w-full md:w-auto justify-end">
             <button type="button" onclick="openModal('exportModal')" class="bg-gray-200 text-gray-700 px-4 py-2 rounded-lg text-sm font-medium hover:bg-gray-300 w-full md:w-auto">
-                <i class="fas fa-file-export me-2"></i>Laporan Export
+                <i class="fas fa-file-export me-2"></i>Export
             </button>
             <a href="{{ route('admin.pesanan.create') }}" class="bg-red-600 text-white px-4 py-2 rounded-lg text-sm font-medium hover:bg-red-700 w-full md:w-auto text-center">
                 <i class="fas fa-plus me-2"></i>Buat Pesanan Baru
