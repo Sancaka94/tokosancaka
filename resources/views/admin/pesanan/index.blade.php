8a25aff805007cbc489d4d1dfd97c6a5e513a625
Admin Sancaka <admin@tokosancaka.com>
2025-12-25 16:47:54 +0700

    auto update

--- resources/views/admin/pesanan/index.blade.php
+++ resources/views/admin/pesanan/index.blade.php
@@ -95,7 +95,7 @@
                 <i class="fas fa-file-export me-2"></i>Export
             </button>
             <a href="{{ route('admin.pesanan.create') }}" class="bg-red-600 text-white px-4 py-2 rounded-lg text-sm font-medium hover:bg-red-700 w-full md:w-auto text-center">
-                <i class="fas fa-plus me-2"></i>Tambah Pesanan
+                <i class="fas fa-plus me-2"></i>Buat Pesanan Baru
             </a>
         </div>
     </div>
