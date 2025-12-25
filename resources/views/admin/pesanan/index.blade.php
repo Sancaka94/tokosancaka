e803b0b304f19f0898bbecf398f052313baa5a4d
Admin Sancaka <admin@tokosancaka.com>
2025-12-25 16:56:43 +0700

    auto update

--- resources/views/admin/pesanan/index.blade.php
+++ resources/views/admin/pesanan/index.blade.php
@@ -96,7 +96,6 @@
             </button>
             <a href="{{ route('admin.pesanan.create') }}" class="bg-red-600 text-white px-4 py-2 rounded-lg text-sm font-medium hover:bg-red-700 w-full md:w-auto text-center">
                 <i class="fas fa-plus me-2"></i>Buat Pesanan Baru
-                
             </a>
         </div>
     </div>
