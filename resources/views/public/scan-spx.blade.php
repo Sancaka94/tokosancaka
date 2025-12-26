dc2774e352ca7773dc48d4214ca1326fe8efb2de
Admin Sancaka <admin@tokosancaka.com>
2025-12-26 20:56:58 +0700

    auto update

--- resources/views/public/scan-spx.blade.php
+++ resources/views/public/scan-spx.blade.php
@@ -225,7 +225,7 @@
                 <ul class="promo-list">
                     <li><i class="fas fa-check-circle"></i> Akses Full Fitur Aplikasi Sancaka.</li>
                     <li><i class="fas fa-store"></i> <a href="https://tokosancaka.com/etalase" target="_blank" style="text-decoration: underline; color: inherit;"><strong>Berjualan di Marketplace Sancaka</strong></a> (Jangkauan Luas).</li>
-                    <li><i class="fas fa-wallet"></i> Jadi AGEN Loket PPOB (Jual Pulsa, Token Listrik, Bayar Air PDAM, dll).</li>
+                    <li><i class="fas fa-wallet"></i> Jadi AGEN Loket PPOB Dengan Harga Kulak Kompetitif dan Realtime (Jual Pulsa, Token Listrik, Bayar Air PDAM, dll).</li>
                     <li><i class="fas fa-chart-line"></i> Monitor Jumlah Kiriman Paket ALL Ekpedisi & SPX Realtime.</li>
                     <li><i class="fas fa-search-location"></i> Lacak Status & Surat Jalan SPX dengan Detail.</li>
                 </ul>
