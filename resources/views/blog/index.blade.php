82dc8ce114a7204551bd0a3640133819d11e8b97
Admin Sancaka <admin@tokosancaka.com>
2025-12-31 10:36:53 +0700

    auto update

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -245,9 +245,12 @@
                 </div>
             @endforelse
             
-            {{-- Pagination --}}
-            <div class="col-12 mt-4">
-                {{ $latestPosts->links() }}
+            <div class="mt-8 px-4 w-full flex justify-center">
+    {{-- 
+        onEachSide(1): Membatasi jumlah angka di HP agar tidak meluber.
+        links(): Akan otomatis merender HTML Tailwind yang cantik.
+    --}}
+    {{ $latestPosts->onEachSide(1)->links() }}
             </div>
         </div>
 
