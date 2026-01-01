b473cd07b1008898433cda0ccc89b0be97f1d0fa
Admin Sancaka <admin@tokosancaka.com>
2026-01-01 09:05:00 +0700

    auto update

--- ponpes/bootstrap/app.php
+++ ponpes/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Auth; // Tambahkan ini
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        
+        // Logika Redirect untuk User yang sudah login (pengganti RedirectIfAuthenticated)
+        $middleware->redirectUsersTo(function () {
+            $user = Auth::user();
+
+            if ($user && $user->role === 'admin') {
+                return route('admin.dashboard'); // Pastikan name route ini ada di web.php
+            }
+
+            return route('dashboard'); // Redirect default untuk user biasa
+        });
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
