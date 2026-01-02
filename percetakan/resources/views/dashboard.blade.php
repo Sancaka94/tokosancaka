eeb05a7411b5439b10b1d469e6c0a8bad5627c88
Admin Sancaka <admin@tokosancaka.com>
2026-01-02 19:21:01 +0700

    auto update

--- percetakan/resources/views/dashboard.blade.php
+++ percetakan/resources/views/dashboard.blade.php
@@ -43,16 +43,7 @@
         <div class="flex-1 flex flex-col min-w-0 overflow-hidden">
             
             <header class="bg-white border-b border-gray-200 h-16 flex items-center justify-between px-4 lg:px-6">
-                <div class="flex items-center gap-4">
-                    <button @click="sidebarOpen = !sidebarOpen" class="text-gray-500 hover:text-gray-700 focus:outline-none lg:hidden">
-                        <i class="fas fa-bars text-lg"></i>
-                    </button>
-                    <h1 class="text-lg font-semibold text-gray-700 hidden sm:block">Dashboard</h1>
-                </div>
-
-                    <div class="flex items-center gap-4">
-                    @include('layouts.partials.header')
-                    </div>
+                    <div class="flex items-center gap-4">@include('layouts.partials.header')</div>
             </header>
 
             <main class="flex-1 overflow-y-auto p-4 lg:p-6">
