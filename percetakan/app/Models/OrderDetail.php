c67ab3ad06da4a2b1ad7e7f1c7fa78789d05ae01
Admin Sancaka <admin@tokosancaka.com>
2026-01-02 21:18:35 +0700

    auto update

--- percetakan/app/Models/OrderDetail.php
+++ percetakan/app/Models/OrderDetail.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
+    use HasFactory;
+
+    public $timestamps = true; // Sekarang tabel sudah punya kolom waktu
+
     protected $fillable = [
-        'order_id', 'product_id', 'price_at_order', 'quantity', 'subtotal'
+        'order_id',
+        'product_id',
+        'product_name',   // Kolom baru
+        'price_at_order', // SESUAI DATABASE ANDA (bukan 'price')
+        'quantity',       // SESUAI DATABASE ANDA (bukan 'qty', lihat gambar image_d0c72b.png)
+        'subtotal',
+        'file_design',    // Sesuai gambar database
+        'width',
+        'height'
     ];
 
     public function product()
