5c2980ae1d79602cf4a598b170c996bb2b313b24
Admin Sancaka <admin@tokosancaka.com>
2026-01-05 20:58:57 +0700

    auto update

--- percetakan/resources/views/orders/create.blade.php
+++ percetakan/resources/views/orders/create.blade.php
@@ -54,10 +54,7 @@
                 </button>
             </div>
 
-            <div class="flex-1 overflow-y-auto p-4 custom-scrollbar bg-slate-50">
-                <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 xl:grid-cols-4 gap-3">
-                    
-                    <div x-data="{ showInfo: true }" 
+            <div x-data="{ showInfo: true }" 
      x-show="showInfo" 
      x-transition.opacity.duration.300ms
      class="mb-4 bg-green-50 border border-green-200 rounded-xl p-3 flex items-start gap-3 shadow-sm relative group">
@@ -77,7 +74,11 @@
     <button @click="showInfo = false" class="absolute top-2 right-2 text-green-400 hover:text-green-700 hover:bg-green-100 rounded-full h-6 w-6 flex items-center justify-center transition-all">
         <i class="fas fa-times text-xs"></i>
     </button>
-</div>
+    </div>
+
+            <div class="flex-1 overflow-y-auto p-4 custom-scrollbar bg-slate-50">
+                <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 xl:grid-cols-4 gap-3">
+                    
                     @forelse($products as $product)
                     <template x-if="itemMatchesSearch('{{ addslashes($product->name) }}')">
                         <div @click="addToCart({{ $product->id }}, '{{ addslashes($product->name) }}', {{ $product->sell_price }}, {{ $product->stock }}, {{ $product->weight ?? 0 }})"
