d6edc8647b6d6438069c1eed3647dadcdfdf5d06
Admin Sancaka <admin@tokosancaka.com>
2026-01-08 21:49:50 +0700

    auto update

--- percetakan/routes/web.php
+++ percetakan/routes/web.php
@@ -44,6 +44,8 @@ Route::prefix('dana')->name('dana.')->group(function () {
     // Digunakan untuk eksekusi kirim uang ke user
     Route::post('/topup', [DanaDashboardController::class, 'topupSaldo'])->name('topup');
 
+    Route::post('/dana/topup', [DanaDashboardController::class, 'customerTopup'])->name('dana.topup');
+
     Route::post('/account-inquiry', [DanaDashboardController::class, 'accountInquiry'])->name('account_inquiry');
 });
 
