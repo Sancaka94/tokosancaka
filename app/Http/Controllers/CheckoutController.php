50776cbcd29845e7be0e7457eaac2fd80db5c21b
Admin Sancaka <admin@tokosancaka.com>
2026-01-15 00:25:13 +0700

    auto update

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -304,7 +304,7 @@ class CheckoutController extends Controller
             }
         }
 
-        return view('checkout.index', compact('cart', 'expressOptions', 'instantOptions', 'user'));
+        return view('checkout.index', compact('cart', 'expressOptions', 'instantOptions', 'user', 'weblogo'));
     }
 
 
