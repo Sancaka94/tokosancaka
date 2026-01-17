703401a18dd72ad0678a0b0628837e72d3ca90b5
Admin Sancaka <admin@tokosancaka.com>
2026-01-17 18:07:59 +0700

    auto update

--- percetakan/resources/views/orders/create.blade.php
+++ percetakan/resources/views/orders/create.blade.php
@@ -385,7 +385,8 @@
                                     x-model="customerPhone"
                                     x-on:input="customerPhone = customerPhone.replace(/[^0-9]/g, '')"
                                     x-on:blur="sanitizePhone()"
-                                class="w-full pl-8 pr-3 py-2 text-xs rounded-lg border border-slate-200 focus:ring-indigo-500 focus:border-indigo-500 bg-slate-50 placeholder-slate-400 transition-all">
+
+                                    class="w-full pl-8 pr-3 py-2 text-xs rounded-lg border border-slate-200 focus:ring-indigo-500 focus:border-indigo-500 bg-slate-50 placeholder-slate-400 transition-all">
                             </div>
                         </div>
 
