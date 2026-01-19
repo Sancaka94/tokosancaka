2c88647ad949ce5140b6b6e9d89726029f0b60c9
Admin Sancaka <admin@tokosancaka.com>
2026-01-19 20:08:52 +0700

    auto update

--- percetakan/resources/views/layouts/app.blade.php
+++ percetakan/resources/views/layouts/app.blade.php
@@ -54,6 +54,7 @@
                 console.error("❌ Error Config:", err);
             }
         }
+
     </script>
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
