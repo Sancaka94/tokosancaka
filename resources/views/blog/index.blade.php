a9d1b872bd9fe79f354dc5f2e927ff87a12e7f8f
Admin Sancaka <admin@tokosancaka.com>
2026-01-24 12:11:00 +0700

    auto update

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,28 +4,32 @@
 
 @section('content')
 
-{{-- ASSETS FONT --}}
+{{-- FONT ASSETS --}}
 <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@400;600;700&family=Inter:wght@400;600;700;800&display=swap" rel="stylesheet">
+
+{{-- 1. TICKER --}}
+@include('blog.partials.ticker')
 
 <div class="container py-5">
 
-    {{-- ============================================= --}}
-    {{-- LOOPING SEMUA KATEGORI DENGAN DESAIN SAMA --}}
-    {{-- ============================================= --}}
-
-    @foreach($categories as $category)
-        {{-- Pastikan kategori punya postingan agar tidak kosong --}}
-        @if($category->posts()->count() > 0)
-
-            {{-- Panggil Desain SmartMag untuk setiap kategori --}}
-            @include('blog.partials.categories_smartmag', ['category' => $category])
-
-            {{-- Pemisah antar kategori --}}
-            <div style="border-top: 1px dashed #ddd; margin: 40px 0;"></div>
-
-        @endif
+    {{-- 2. KATEGORI UTAMA (Desain Grid Kiri + Sidebar) --}}
+    @foreach($categories->take(1) as $category)
+        @include('blog.partials.categories_smartmag', ['category' => $category])
     @endforeach
+
+    {{-- SPACER --}}
+    <div style="border-top: 1px dashed #ddd; margin: 50px 0;"></div>
+
+    {{-- 3. BOTTOM GRID 4 KOLOM (Desain Travel, UK, Science, Economy) --}}
+    {{-- Ini akan menampilkan 4 kategori berikutnya secara berjajar --}}
+    @include('blog.partials.bottom_grid')
 
 </div>
 
+@endsection
+
+{{-- 4. FOOTER (Sebaiknya letakkan kode ini di file layouts/blog.blade.php menggantikan footer lama) --}}
+{{-- Tapi jika ingin dipanggil di sini juga bisa, pastikan diluar container content utama --}}
+@section('footer')
+    @include('blog.partials.footer_smartmag')
 @endsection
