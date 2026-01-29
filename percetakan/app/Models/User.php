089dac3c486f2f0c293eeb60a63b6afbb61d3002
Admin Sancaka <admin@tokosancaka.com>
2026-01-29 11:51:57 +0700

    auto update

--- percetakan/app/Models/User.php
+++ percetakan/app/Models/User.php
@@ -6,12 +6,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Traits\BelongsToTenant; // <-- Pastikan ini di-import
+
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
+    use BelongsToTenant; // <-- Pastikan ini dipasang di dalam class
     /**
      * The attributes that are mass assignable.
      *
