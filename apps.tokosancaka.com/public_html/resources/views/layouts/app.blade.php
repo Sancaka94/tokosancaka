4f1434a209524859eef06a76301d9a35906e432e
Admin Sancaka <admin@tokosancaka.com>
2026-02-08 18:51:14 +0700

    Update System

--- apps.tokosancaka.com/public_html/resources/views/layouts/app.blade.php
+++ apps.tokosancaka.com/public_html/resources/views/layouts/app.blade.php
@@ -13,6 +13,10 @@
     <script src="https://unpkg.com/lucide@latest"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
+
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
     {{-- Asset Echo & Pusher tetap di sini sesuai permintaan (Jangan dirubah) --}}
     <script src="{{ asset('libs/pusher.min.js') }}"></script>
     <script src="{{ asset('libs/echo.js') }}"></script>
