a04c015318bd7d483b0789d20a8ad631732ddea4
Admin Sancaka <admin@tokosancaka.com>
2026-02-13 18:15:04 +0700

    Update System

--- apps.tokosancaka.com/public_html/routes/web.php
+++ apps.tokosancaka.com/public_html/routes/web.php
@@ -600,4 +600,5 @@ Route::middleware('auth')->group(function () {
 Route::get('/orders/tripay-channels', [OrderController::class, 'tripayChannels'])->name('orders.tripay-channels');
 
 // Route untuk Cek Status Transaksi DANA
-Route::post('/member/dana/check-status', [App\Http\Controllers\MemberAuthController::class, 'checkTopupStatus'])->name('dana.checkTopupStatus');
+Route::post('/member/dana/check-status', [DanaGatewayController::class, 'checkTopupStatus'])
+    ->name('dana.checkTopupStatus');
