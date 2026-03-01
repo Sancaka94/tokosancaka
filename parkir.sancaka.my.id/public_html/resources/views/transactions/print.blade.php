f7395a5d7ae57807ceb2fdc09273e98760e3810c
Admin Sancaka <admin@tokosancaka.com>
2026-03-01 14:22:38 +0700

    Update System

--- parkir.sancaka.my.id/public_html/resources/views/transactions/print.blade.php
+++ parkir.sancaka.my.id/public_html/resources/views/transactions/print.blade.php
@@ -1,7 +1,8 @@
 <?php
 // Susun teks murni dengan bahasa format RawBT
-$text = "[C]SANCAKA PARKIR\n";
-$text .= "[C]Jl. Dr. Wahidin No. 18A, Ngawi\n";
+$text = "AZKEN PARKIR\n";
+$text .= "Jl. Dr. Wahidin No. 18A, Ngawi\n";
+$text .= "Nomor WA 085 745 808 809\n";
 $text .= "--------------------------------\n";
 $text .= "No. Plat : " . $transaction->plate_number . "\n";
 $text .= "Jenis    : " . ucfirst($transaction->vehicle_type) . "\n";
@@ -9,13 +10,13 @@ $text .= "Masuk    : " . $transaction->entry_time->format('d/m/Y H:i') . "\n";
 $text .= "--------------------------------\n";
 
 // Ajaibnya RawBT: Kode [QR] di bawah ini otomatis diubah jadi gambar QR Code oleh printer!
-$text .= "[C][QR]" . $transaction->id . "\n";
+$text .= "[QR]" . $transaction->id . "\n";
 
-$text .= "[C]TRX-" . str_pad($transaction->id, 5, '0', STR_PAD_LEFT) . "\n";
+$text .= "TRX-" . str_pad($transaction->id, 5, '0', STR_PAD_LEFT) . "\n";
 $text .= "--------------------------------\n";
-$text .= "[C]Simpan karcis ini sebagai\n";
-$text .= "[C]bukti parkir yang sah.\n";
-$text .= "[C]Terima Kasih.\n";
+$text .= "Simpan karcis ini sebagai\n";
+$text .= "bukti parkir yang sah.\n";
+$text .= "Terima Kasih Ya Kak.\n";
 
 // Tambahkan spasi kosong di bawah agar kertas agak naik saat dipotong
 $text .= "\n\n";
