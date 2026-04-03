112b833e2903820f12999ca022891efa98e54b13
Admin Sancaka <admin@tokosancaka.com>
2026-04-03 09:42:45 +0700

    Update System

--- tokosancaka.com/public_html/routes/api.php
+++ tokosancaka.com/public_html/routes/api.php
@@ -222,6 +222,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
 });
 
+// Memanggil rute khusus aplikasi Mobile Sancaka
 if(file_exists(__DIR__.'/api.mobile.app.php')) {
     require __DIR__.'/api.mobile.app.php';
 }
