a847bf047fbddd895bca7aa1e1bbd0b72995fe99
Amal Ibnu Muharram <salafy94@gmail.com>
2026-04-24 20:26:05 +0700

    Auto commit

--- tokosancaka.com/public_html/resources/views/admin/perizinan/index.blade.php
+++ tokosancaka.com/public_html/resources/views/admin/perizinan/index.blade.php
@@ -13,7 +13,7 @@
             </a>
         </div>
 
-        @f(session('success'))
+        @if(session('success'))
             <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
                 <span class="block sm:inline">{{ session('success') }}</span>
             </div>
