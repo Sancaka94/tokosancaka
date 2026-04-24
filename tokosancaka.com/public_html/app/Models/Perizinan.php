9709884d3f3b2c5d6a2bac44d37df55e74d69291
Amal Ibnu Muharram <salafy94@gmail.com>
2026-04-24 20:38:42 +0700

    Auto commit

--- tokosancaka.com/public_html/app/Models/Perizinan.php
+++ tokosancaka.com/public_html/app/Models/Perizinan.php
@@ -20,8 +20,19 @@ class Perizinan extends Model
         'jenis_bangunan',
         'lokasi',
         'jumlah_lantai',
+        'jumlah_penghuni',      // <-- TAMBAHAN BARU
+        'memiliki_basement',    // <-- TAMBAHAN BARU
         'fungsi_bangunan',
         'legalitas_saat_ini',
+        'status_tanah',         // <-- TAMBAHAN BARU
         'status_krk',
+        'rekom_dishub',         // <-- TAMBAHAN BARU
+        'rekom_damkar',         // <-- TAMBAHAN BARU
+        'andalalin',            // <-- TAMBAHAN BARU
+        'lingkungan',           // <-- TAMBAHAN BARU
+        'nib',                  // <-- TAMBAHAN BARU
+        'siup',                 // <-- TAMBAHAN BARU
+        'perizinan_lain',       // <-- TAMBAHAN BARU
     ];
+
 }
