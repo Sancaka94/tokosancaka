448846f529ccc0a9891e06e6e73221a81a111350
Amal Ibnu Muharram <salafy94@gmail.com>
2026-05-02 17:14:47 +0700

    Auto commit

--- bolone.sancaka.my.id/public_html/resources/views/dashboard.blade.php
+++ bolone.sancaka.my.id/public_html/resources/views/dashboard.blade.php
@@ -6,6 +6,7 @@
     <title>Dashboard Analitik</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.9.0/proj4.js"></script>
     <script src="https://code.highcharts.com/maps/highmaps.js"></script>
     <script src="https://code.highcharts.com/mapdata/countries/id/id-all.js"></script>
 </head>
