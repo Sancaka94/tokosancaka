2a1179efd2f9ba731a6691ffed5d41d8aac42164
Sancaka94 <tokosancaka@gmail.com>
2026-05-12 21:40:28 +0700

    Auto commit

--- tokosancaka.com/public_html/app/Http/Controllers/Api/Mobile/TicketingController.php
+++ tokosancaka.com/public_html/app/Http/Controllers/Api/Mobile/TicketingController.php
@@ -70,7 +70,7 @@ class TicketingController extends BaseController
         return $response;
     }
 
-    /**
+   /**
      * POST Airline/Price
      * Endpoint untuk mengecek harga detail dari maskapai berdasarkan jadwal yang dipilih
      */
@@ -97,7 +97,30 @@ class TicketingController extends BaseController
         // 2. Siapkan Data Payload
         $payload = $request->all();
 
-        // 3. Kirim Request ke Server Darmawisata (BaseController akan otomatis injek userID & accessToken)
+        // ======================================================================
+        // FIX PENTING: Cegah Laravel mengirim 'null' ke Darmawisata
+        // Middleware Laravel mengubah "" jadi null, kita kembalikan lagi jadi ""
+        // ======================================================================
+        if (isset($payload['schDeparts']) && is_array($payload['schDeparts'])) {
+            foreach ($payload['schDeparts'] as $key => $depart) {
+                $payload['schDeparts'][$key]['flightClass'] = $depart['flightClass'] ?? "";
+                $payload['schDeparts'][$key]['detailSchedule'] = $depart['detailSchedule'] ?? "";
+                $payload['schDeparts'][$key]['garudaNumber'] = $depart['garudaNumber'] ?? "";
+                $payload['schDeparts'][$key]['garudaAvailability'] = $depart['garudaAvailability'] ?? "";
+            }
+        }
+
+        if (isset($payload['schReturns']) && is_array($payload['schReturns'])) {
+            foreach ($payload['schReturns'] as $key => $return) {
+                $payload['schReturns'][$key]['flightClass'] = $return['flightClass'] ?? "";
+                $payload['schReturns'][$key]['detailSchedule'] = $return['detailSchedule'] ?? "";
+                $payload['schReturns'][$key]['garudaNumber'] = $return['garudaNumber'] ?? "";
+                $payload['schReturns'][$key]['garudaAvailability'] = $return['garudaAvailability'] ?? "";
+            }
+        }
+        // ======================================================================
+
+        // 3. Kirim Request ke Server Darmawisata
         $response = $this->forwardRequest('Airline/Price', $payload);
 
         // Opsional: Log response jika butuh untuk debug
