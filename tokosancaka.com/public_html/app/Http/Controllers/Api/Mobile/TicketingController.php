3a901d552400d03400095641c198bea585f6ae7e
Sancaka94 <tokosancaka@gmail.com>
2026-05-12 23:38:05 +0700

    Auto commit

--- tokosancaka.com/public_html/app/Http/Controllers/Api/Mobile/TicketingController.php
+++ tokosancaka.com/public_html/app/Http/Controllers/Api/Mobile/TicketingController.php
@@ -160,7 +160,7 @@ class TicketingController extends BaseController
      * POST Airline/Booking
      * Endpoint untuk proses booking tiket pesawat
      */
-    public function airlineBooking(Request $request)
+    public function airlineBooking(Request $request) {
     {
         // 1. Validasi Parameter Dasar (Opsional namun disarankan untuk mencegah hit kosong ke Darmawisata)
         $validator = Validator::make($request->all(), [
