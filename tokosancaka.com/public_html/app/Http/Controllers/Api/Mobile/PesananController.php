33530e020e02f8f5265fe84a717297ec1ed22560
Sancaka94 <tokosancaka@gmail.com>
2026-05-27 14:23:17 +0700

    Auto commit

--- tokosancaka.com/public_html/app/Http/Controllers/Api/Mobile/PesananController.php
+++ tokosancaka.com/public_html/app/Http/Controllers/Api/Mobile/PesananController.php
@@ -319,7 +319,7 @@ class PesananController extends Controller
                     $transaction = Transaction::create([
                         'user_id' => $user->id_pengguna,
                         'amount' => $finalPriceDB,
-                        'type' => 'pesanan',
+                        'type' => 'order',
                         'status' => 'pending',
                         'payment_method' => str_replace('#', '', $validatedData['payment_method']), // Bersihkan tanda # saat masuk DB
                         'reference_id' => $order->nomor_invoice,
