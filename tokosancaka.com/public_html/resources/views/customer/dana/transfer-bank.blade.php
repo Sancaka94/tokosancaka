a835f41f6a5515b0d22432159f68b469d12f3415
Amal Ibnu Muharram <salafy94@gmail.com>
2026-05-29 17:44:16 +0700

    Fix

--- tokosancaka.com/public_html/resources/views/customer/dana/transfer-bank.blade.php
+++ tokosancaka.com/public_html/resources/views/customer/dana/transfer-bank.blade.php
@@ -39,7 +39,7 @@
                             @endforeach
                         </ul>
                     </div>
-                
+                @endif
 
                 @if (session('error'))
                     <div class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
