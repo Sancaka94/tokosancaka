a688f7ae3557a3340482bfa825911001e5ee8550
Amal Ibnu Muharram <salafy94@gmail.com>
2026-05-30 16:09:18 +0700

    Auto commit update

--- tokosancaka.com/public_html/routes/web.php
+++ tokosancaka.com/public_html/routes/web.php
@@ -1406,7 +1406,6 @@ Route::prefix('customer')->name('customer.')->middleware(['auth'])->group(functi
     
     Route::get('/topup-dana', [TopupDanaController::class, 'create'])->name('topupdana.create');
     Route::post('/topup-dana', [TopupDanaController::class, 'store'])->name('topupdana.store');
-    Route::post('/dana/topup-dana/cek-status', [App\Http\Controllers\Customer\TopupDanaController::class, 'checkStatus'])->name('topupdana.check_status');
     
     // Rute Halaman Sukses (Kembalian dari DOKU/Tripay setelah bayar)
     Route::get('/topup-dana/success/{invoice?}', function($invoice = null) {
@@ -1415,3 +1414,5 @@ Route::prefix('customer')->name('customer.')->middleware(['auth'])->group(functi
     })->name('topupdana.success');
 
 });
+
+Route::post('/dana/topup-dana/cek-status', [App\Http\Controllers\Customer\TopupDanaController::class, 'checkStatus'])->name('topupdana.check_status');
