47ea0fba04492b00040adcbe0dda12ba377e5424
Sancaka94 <tokosancaka@gmail.com>
2026-07-02 23:48:59 +0700

    Auto commit

--- tokosancaka.com/public_html/app/Http/Controllers/Api/Mobile/ApiMapboxController.php
+++ tokosancaka.com/public_html/app/Http/Controllers/Api/Mobile/ApiMapboxController.php
@@ -429,11 +429,11 @@ class ApiMapboxController extends Controller
      */
     private function getDistanceMeter($lat1, $lon1, $lat2, $lon2) {
         $earthRadius = 6371000; // Radius bumi dalam meter
-        $latDelta = radians($lat2 - $lat1);
-        $lonDelta = radians($lon2 - $lon1);
+        $latDelta = deg2rad($lat2 - $lat1);
+        $lonDelta = deg2rad($lon2 - $lon1);
 
         $a = sin($latDelta / 2) * sin($latDelta / 2) +
-             cos(radians($lat1)) * cos(radians($lat2)) *
+             cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
              sin($lonDelta / 2) * sin($lonDelta / 2);
         $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
         return round($earthRadius * $c);
