f53c60b2c8021d25122f050ce434d76b8c650522
Sancaka94 <tokosancaka@gmail.com>
2026-07-25 13:17:51 +0700

    Auto commit

--- tokosancaka.com/public_html/routes/api.mobile.app.php
+++ tokosancaka.com/public_html/routes/api.mobile.app.php
@@ -54,7 +54,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/order/notify-driver', [ApiMapboxController::class, 'notify_driver']);
     Route::post('/order/driver-accept', [ApiMapboxController::class, 'accept_order']);
     Route::get('/order/track-driver/{driver_id}', [ApiMapboxController::class, 'track_driver']);
-    Route::get('/order/detail/{order_id}', [ApiMapboxController::class, 'get_order_detail']);
+Route::get('/order/resi/{order_id}', [ApiMapboxController::class, 'get_order_resi_detail']);
     Route::get('/order/resi/{order_id}', [ApiMapboxController::class, 'get_order_resi_detail']);
     Route::post('/order/update-status', [ApiMapboxController::class, 'update_status_order']);
     Route::get('/order/history', [ApiMapboxController::class, 'get_history']);
