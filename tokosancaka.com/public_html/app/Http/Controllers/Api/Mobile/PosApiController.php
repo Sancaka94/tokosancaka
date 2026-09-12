44fcf112b8d146304c8100166c1bf9cd91786a43
Sancaka94 <tokosancaka@gmail.com>
2026-09-12 14:42:29 +0700

    Auto commit

--- tokosancaka.com/public_html/app/Http/Controllers/Api/Mobile/PosApiController.php
+++ tokosancaka.com/public_html/app/Http/Controllers/Api/Mobile/PosApiController.php
@@ -352,7 +352,7 @@ class PosApiController extends Controller
         // WAJIB UNTUK QRIS: Tambahkan externalStoreId dan tentukan payMethod
         if (strtoupper($paymentMethod) === 'QRIS') {
             // Gunakan Store ID dari DANA Sandbox/Production Anda
-            $body['externalStoreId'] = config('services.dana.external_store_id', '20050811210214220002'); 
+            $body['externalStoreId'] = config('services.dana.external_store_id', '216620050026029338146'); 
             $body['payOptionDetails'] = [
                 [
                     "payMethod" => "QR_CODE"
@@ -459,7 +459,7 @@ class PosApiController extends Controller
         
         // Ambil konfigurasi
         $merchantId = config('services.dana.merchant_id');
-        $storeId = config('services.dana.external_store_id', '20050811210214220002');
+        $storeId = config('services.dana.external_store_id', '216620050026029338146');
 
         $body = [
             "merchantId"         => $merchantId,
